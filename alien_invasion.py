1dacdbfce4188b184b1c292b9e0473e4b094c680
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2025-01-30 13:10:58 -0500

    updated code to update ship movement up and down

--- alien_invasion.py
+++ alien_invasion.py
@@ -60,6 +60,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
             
@@ -69,6 +73,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
 if __name__ == '__main__':
     # make a game instance and run the game
--- ship.py
+++ ship.py
@@ -17,12 +17,18 @@ class Ship:
         # start each new ship at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
         
-        # store a float for the ships exact horizontal position
+        
+        # store a float for the ships exact horizontal/vertical position
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        
+        
         
         # movement flag; start with a ship that isn't moving
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
@@ -34,9 +40,14 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
         
         # update rect object from self.x    
         self.rect.x = self.x
+        self.rect.y = self.y
             
     def blitme(self):
         """Draw the ship at its current location."""
