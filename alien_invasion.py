0896807decd3e1b6427f35c85999856e970be36a
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-29 22:50:53 -0500

    added ship image file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame  # type: ignore
+from settings import Settings
 
 
 class AlienInvasion:
@@ -8,12 +9,15 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
-        
-        # Set the background color
-        self.bg_color = (230, 230, 230)
+        # initialize clock method for refresh rate
         self.clock = pygame.time.Clock()
+
+        # initialize settings from settings.py
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Start main loop for game."""
@@ -24,7 +28,7 @@ class AlienInvasion:
                     sys.exit()
 
             # redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # make the most recently drawn screen visible
             pygame.display.flip()
