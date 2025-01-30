bbeb58dd83f9907d8592a3466ffe860166865e9c
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-30 16:28:43 -0500

    created fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,6 +38,15 @@ class AlienInvasion:
         """Create a fleet of aliens."""
         # make an alien
         alien = Alien(self)
+        alien_width = alien.rect.width
+        current_x = alien_width
+
+        while current_x < (self.settings.screen_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
         # add the alien to the sprite group
         self.aliens.add(alien)
