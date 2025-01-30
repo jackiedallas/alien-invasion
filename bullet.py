f2927ce984d36b9f197c1a54ab5588f48ad34722
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2025-01-30 13:27:13 -0500

    created draw bullet method in bullet class

--- bullet.py
+++ bullet.py
@@ -24,3 +24,7 @@ class Bullet(Sprite):
         self.y -= self.settings.bullet_speed
         # update the rect position
         self.rect.y = self.y
+    
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
