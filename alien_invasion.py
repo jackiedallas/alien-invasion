ab4d089bea3271cb10038aa6ba0b681383d7b034
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-30 16:31:00 -0500

    added two aliens widths to end of screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,7 +41,7 @@ class AlienInvasion:
         alien_width = alien.rect.width
         current_x = alien_width
 
-        while current_x < (self.settings.screen_width):
+        while current_x < (self.settings.screen_width - 2 * alien_width):
             new_alien = Alien(self)
             new_alien.x = current_x
             new_alien.rect.x = current_x
